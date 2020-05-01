87e0509e1714348a6604f1fd085c2c6ac0962912
templexxx <lucas1x1x@gmail.com>
2020-05-01 16:32:10 +0800

    [feature] add instanceid package

--- go.mod
+++ go.mod
@@ -5,9 +5,9 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/docker/go-units v0.4.0
+	github.com/google/uuid v1.1.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/pingcap/check v0.0.0-20200212061837-5e12011dc712
-	github.com/pingcap/log v0.0.0-20191012051959-b742a5d432e9
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.6.0
 	github.com/templexxx/logro v0.0.2
---
+++ uid/instanceid/instanceid.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020. Temple3x (temple3x@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package instanceid provides method to generate unique Instance ID.
+// Any ZBuffer and ZStore instance need an unique Instance ID within a box.
+package instanceid
+
+import (
+	"encoding/hex"
+
+	"github.com/google/uuid"
+)
+
+// Get gets Instance ID (according MAC address).
+//
+// Warn:
+// It maybe not unique when:
+// 1. In container, MAC address maybe not unique in cluster.
+//   (There are still ways to make it unique, try to make it and things will get easier)
+// 2. After replacing a new net interface, the MAC address won't be as same as before.
+//   (You should offline instance first if hardware need to be replaced.)
+func Get() string {
+	return hex.EncodeToString(uuid.NodeID())
+}
